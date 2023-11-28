f479b5e578d23fcf3930f133cb0b29450d13eb39
Mick Johnson <michaeljohnson@Micks-MacBook-Pro.local>
2023-11-27 20:01:18 -0600

    mobile stillg

--- src/modules/GamePage.js
+++ src/modules/GamePage.js
@@ -202,7 +202,7 @@ export default function GamesPage({}) {
         <Text fontWeight="semibold">Score</Text>
       </Grid>
       <Box
-        paddingBottom={16}
+        paddingBottom={{ base: 24, md: 4 }}
         overflowY="auto"
         height="calc(100vh - 5rem)"
         position="relative"
